02fe733e2ae771b63e6dac4301d16acd2bb1b6f0
mhatikov <kramskov.mgoo@gmail.com>
2023-05-06 16:42:29 +0400

    components #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { PromotionBannerComponent } from './components/promotion-banner/promotion-banner.component';
+import { AuthorisationPageComponent } from './components/authorisation-page/authorisation-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    HeaderComponent,
+    SliderComponent,
+    PromotionBannerComponent,
+    AuthorisationPageComponent
   ],
   imports: [
     BrowserModule,
