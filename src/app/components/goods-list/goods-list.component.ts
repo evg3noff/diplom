323696b519af899c2d511c39dd64a56505b23d5b
mhatikov <kramskov.mgoo@gmail.com>
2023-05-09 23:49:51 +0400

    goods list add

---
+++ src/app/components/goods-list/goods-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goods-list',
+  templateUrl: './goods-list.component.html',
+  styleUrls: ['./goods-list.component.scss']
+})
+export class GoodsListComponent {
+
+}
