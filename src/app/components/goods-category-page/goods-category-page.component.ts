4f9d96a263404afe12623ec8a3f931a96360e3e3
mhatikov <kramskov.mgoo@gmail.com>
2023-05-21 20:49:15 +0400

    feat: тут дохуя чего

---
+++ src/app/components/goods-category-page/goods-category-page.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-goods-category-page',
+  templateUrl: './goods-category-page.component.html',
+  styleUrls: ['./goods-category-page.component.scss']
+})
+export class GoodsCategoryPageComponent implements OnInit, OnDestroy {
+
+  category: undefined | string;
+  unsubscribe$: Subject<void> = new Subject<void>();
+
+  constructor(
+    private route: ActivatedRoute
+  ) {
+
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(res => {
+      this.category = res["GoodsCategory"];
+      console.log(this.category);
+    });
+  }
+}
--- src/app/components/goods-list/goods-list.component.ts
+++ src/app/components/goods-list/goods-list.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { Good } from 'src/app/interfaces/good.interface';
 
 @Component({
   selector: 'app-goods-list',
   templateUrl: './goods-list.component.html',
   styleUrls: ['./goods-list.component.scss']
 })
-export class GoodsListComponent {
+export class GoodsListComponent implements OnInit, OnDestroy, OnChanges {
+  @Input() collectionName: string | undefined;
+  @Input() main: boolean = false;
 
+  @Input() category: string | undefined;
+
+  goods: Good[] | undefined;
+
+  unsubscribe$: Subject<void> = new Subject<void>();
+  private firestore: Firestore = inject(Firestore);
+
+  constructor() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.collectionName) {
+      const userProfileCollection = collection(this.firestore, this.collectionName);
+      const obs = collectionData(userProfileCollection) as Observable<Good[]>;
+
+      obs.pipe(takeUntil(this.unsubscribe$)).subscribe(res => {
+        if (!this.main) {
+          this.goods = res;
+        } else {
+          this.goods = res.filter((el) => el.oldPrice).splice(0, 10).map(el => ({ ...el, img: `/goods/${el.img}` }));
+        }
+
+        if(this.category) {
+          this.goods = res.filter((el) => el.category === this.category).map(el => ({ ...el, img: `/goods/${el.img}` }));;
+        }
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  ngOnInit() {
+    
+  }
 }
